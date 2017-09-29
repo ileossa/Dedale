75ac306a3d6c51a1030c4f67b0374fdbc1bdc398
ileossa <vincnet.iam@gmail.com>
2017-09-29 18:17:52 +0200

    add poc feature camera (take photo, no video)

---
+++ app/src/main/java/babar/application/ileossa/babar/camera/factory/AlbumStorageDirFactory.java
@@ -0,0 +1,7 @@
+package babar.application.ileossa.babar.camera.factory;
+
+import java.io.File;
+
+public abstract class AlbumStorageDirFactory {
+	public abstract File getAlbumStorageDir(String albumName);
+}
---
+++ app/src/main/java/babar/application/ileossa/babar/camera/factory/FroyoAlbumDirFactory.java
@@ -0,0 +1,19 @@
+package babar.application.ileossa.babar.camera.factory;
+
+import android.os.Environment;
+
+import java.io.File;
+
+public final class FroyoAlbumDirFactory extends AlbumStorageDirFactory {
+
+	@Override
+	public File getAlbumStorageDir(String albumName) {
+		// TODO Auto-generated method stub
+		return new File(
+		  Environment.getExternalStoragePublicDirectory(
+		    Environment.DIRECTORY_PICTURES
+		  ), 
+		  albumName
+		);
+	}
+}
